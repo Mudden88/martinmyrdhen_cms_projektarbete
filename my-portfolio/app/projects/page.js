55e9f6fa1ecf2aea42f046ce3b941b7a4e76d8e2
mudden88 <mudden@live.se>
2024-12-29 20:01:12 +0100

    contentful api.js

--- my-portfolio/app/projects/page.js
+++ my-portfolio/app/projects/page.js
@@ -1,11 +1,19 @@
+import Image from "next/image"
 import Link from "next/link"
-export default function Projects() {
+
+export default async function Projects() {
+
   return (
     <div>
       Här kommer projekten
       <p>
         <Link href={"projects/MinFörstaPost"}>Läsmer testpost</Link>
       </p>
+      <div className="space-y-12">
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+
+        </div>
+      </div>
     </div>
   )
 }
