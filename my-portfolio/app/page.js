79d3550cbf0ee5f6c0bd4bd9d8bc054b4e14077f
mudden88 <mudden@live.se>
2025-01-01 16:29:37 +0100

    contentful metadata

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,16 +1,20 @@
 import Link from "next/link";
+import { getMetaData } from "@/lib/api";
 
-export default function Home() {
+export default async function Home() {
 
   const buttonClass = "bg-purple-900 text-white mt-4 py-2 px-4 rounded-lg hover:bg-purple-950 focus:outline-none text-center"
+  const metadata = await getMetaData();
+
 
   return (
     <>
       <section>
+
         <div className="h-60 w-full bg-cover bg-center flex flex-col justify-center"
           style={{ backgroundImage: "url('yaroslav-a-0rSrUYHg5l8-unsplash.jpg')" }}>
-          <h1 className="font-bold text-3xl lg:text-6xl text-white self-center">Contentful Title</h1>
-          <p className="text-white self-center mt-2">Contentful text</p>
+          <h1 className="font-bold text-3xl lg:text-6xl text-white self-center">{metadata[0]?.heroTitle}</h1>
+          <p className="text-white self-center mt-2">{metadata[0]?.heroText}</p>
         </div>
       </section>
       <main className="p-4 flex-auto flex flex-col justify-center">
--- my-portfolio/lib/api.js
+++ my-portfolio/lib/api.js
@@ -1,95 +1,39 @@
 // Set a variable that contains all the fields needed for articles when a fetch for
 // content is performed
-const ARTICLE_GRAPHQL_FIELDS = `
-  sys {
-    id
-  }
-  title
-  slug
-  summary
-  details {
-    json
-    links {
-      assets {
-        block {
-          sys {
-            id
-          }
-          url
-          description
-        }
-      }
-    }
-  }
-  date
-  authorName
-  categoryName
-  articleImage {
-    url
-  }
+const METADATA_GRAPHQL_FIELDS = `
+  siteTitle
+  socialLinks
+  heroTitle
+  heroText
 `;
 
-async function fetchGraphQL(query, preview = false) {
-  return fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Switch the Bearer token depending on whether the fetch is supposed to retrieve live
-        // Contentful content or draft content
-        Authorization: `Bearer ${preview
-            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-            : process.env.CONTENTFUL_ACCESS_TOKEN
-          }`,
-      },
-      body: JSON.stringify({ query }),
-      // Associate all fetches for articles with an "articles" cache tag so content can
-      // be revalidated or updated from Contentful on publish
-      next: { tags: ["articles"] },
-    }
-  ).then((response) => response.json());
+async function fetchGraphQL(query, isDraftMode) {
+  const response = await fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`
+    },
+    body: JSON.stringify({ query })
+  });
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return response.json();
 }
 
-function extractArticleEntries(fetchResponse) {
-  return fetchResponse?.data?.knowledgeArticleCollection?.items;
-}
-
-export async function getAllArticles(
-  // For this demo set the default limit to always return 3 articles.
-  limit = 3,
-  // By default this function will return published content but will provide an option to
-  // return draft content for reviewing articles before they are live
-  isDraftMode = false
-) {
-  const articles = await fetchGraphQL(
+export async function getMetaData(isDraftMode = false) {
+  const metadata = await fetchGraphQL(
     `query {
-        knowledgeArticleCollection(where:{slug_exists: true}, order: date_DESC, limit: ${limit}, preview: ${isDraftMode ? "true" : "false"
-    }) {
-          items {
-            ${ARTICLE_GRAPHQL_FIELDS}
-          }
+      metadataCollection(preview: ${isDraftMode ? "true" : "false"}) {
+        items {
+          ${METADATA_GRAPHQL_FIELDS}
         }
-      }`,
+      }
+    }`,
     isDraftMode
   );
-  return extractArticleEntries(articles);
-}
-
-export async function getArticle(
-  slug,
-  isDraftMode = false
-) {
-  const article = await fetchGraphQL(
-    `query {
-        knowledgeArticleCollection(where:{slug: "${slug}"}, limit: 1, preview: ${isDraftMode ? "true" : "false"
-    }) {
-          items {
-            ${ARTICLE_GRAPHQL_FIELDS}
-          }
-        }
-      }`,
-    isDraftMode
-  );
-  return extractArticleEntries(article)[0];
+  return metadata?.data?.metadataCollection?.items;
 }
