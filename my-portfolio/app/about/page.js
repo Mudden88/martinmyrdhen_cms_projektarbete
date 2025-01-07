9501a0f57ac2edffa97713f99d2c3b8f776c4d22
mudden88 <mudden@live.se>
2025-01-07 11:58:32 +0100

    about

--- my-portfolio/app/about/page.js
+++ my-portfolio/app/about/page.js
@@ -1,5 +1,24 @@
 export default function About() {
   return (
-    <h1>About</h1>
+    <section>
+      <div>
+        <div
+          className='h-60 w-full bg-cover bg-center flex flex-col justify-center'
+          style={{
+            backgroundImage: "url('yaroslav-a-0rSrUYHg5l8-unsplash.jpg')",
+          }}>
+          <h1 className='font-bold text-3xl lg:text-6xl text-white self-center'>
+            About
+          </h1>
+        </div>
+        <main className="p-4 flex flex-col justify-center">
+          <div className="space-y-8">
+            <div className="bg-white shadow-md rounded-lg p-6">
+              <h2 className="font-semibold text-2xl mb-4"></h2>
+            </div>
+          </div>
+        </main>
+      </div>
+    </section>
   )
 }
