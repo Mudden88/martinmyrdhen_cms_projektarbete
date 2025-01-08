c2e1b20fde8f91ca4d4e82e235de1d4431c8649e
mudden88 <mudden@live.se>
2025-01-08 10:48:43 +0100

    contact

--- my-portfolio/app/projects/page.js
+++ my-portfolio/app/projects/page.js
@@ -9,39 +9,52 @@ export default async function Projects() {
     <>
       <section>
         <div
-          className='h-60 w-full bg-cover bg-center flex flex-col justify-center'
+          className="h-60 w-full bg-cover bg-center flex flex-col justify-center"
           style={{
             backgroundImage: "url('yaroslav-a-0rSrUYHg5l8-unsplash.jpg')",
-          }}>
-          <h1 className='font-bold text-3xl lg:text-6xl text-white self-center'>
+          }}
+        >
+          <h1 className="font-bold text-3xl lg:text-6xl text-white self-center">
             Projects
           </h1>
         </div>
       </section>
-      <main className='p-4 flex flex-col justify-center'>
+      <main className="p-4 flex flex-col justify-center">
         <section>
-          <div className='container mx-auto py-8'>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+          <div className="container mx-auto py-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {projects.map((project) => (
                 <div
                   key={project.id}
-                  className='bg-white rounded-lg shadow-md overflow-hidden'>
-                    <div className="relative h-64 w-full">
-                  <Image
-                    src={project?.image?.url}
-                    layout='fill'
-                    objectFit='cover'
-                    alt={project?.image?.title}
-                    className="w-full h-full"></Image>
-</div>
-                  <div className='p-4'>
-                    <h2 className='font-bold text-xl mb-2'>{project?.title}</h2>
+                  className="bg-white rounded-lg shadow-md overflow-hidden"
+                >
+                  <div className="relative h-64 w-full">
+                    <Image
+                      src={project?.image?.url}
+                      layout="fill"
+                      objectFit="cover"
+                      alt={project?.image?.title}
+                      className="w-full h-full"
+                    ></Image>
+                  </div>
+                  <div className="p-4">
+                    <h2 className="font-bold text-xl mb-2">{project?.title}</h2>
                     <p className="text-gray-700">
-                      {project?.details?.json?.content[0].content[0].value?.substring(0, 250)}
-                      {project?.details?.json?.content[0].content[0].value?.length > 250 && '...'}</p>
-                  <Link href={`/projects/${project?.slug}`} className="text-blue-500 hover:underline mt-2 block">Read more</Link>
-                  </div> 
-                </div> 
+                      {project?.details?.json?.content[0].content[0].value?.substring(
+                        0,
+                        250
+                      )}
+                      {project?.details?.json?.content[0].content[0].value
+                        ?.length > 250 && "..."}
+                    </p>
+                    <Link
+                      href={`/projects/${project?.slug}`}
+                      className="text-blue-500 hover:underline mt-2 block"
+                    >
+                      Read more
+                    </Link>
+                  </div>
+                </div>
               ))}
             </div>
           </div>
