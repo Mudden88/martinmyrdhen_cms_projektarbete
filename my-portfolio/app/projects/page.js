7390cca1407a2aef6000b61fc9f92f56dce71109
mudden88 <mudden@live.se>
2025-01-09 09:10:16 +0100

    project page updated with filter categories

--- my-portfolio/app/projects/page.js
+++ my-portfolio/app/projects/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
@@ -11,22 +11,25 @@ export default function Projects() {
   useEffect(() => {
     const fetchProjects = async () => {
       const projectsData = await getAllProjects();
-      console.log("Fetched projects data:", projectsData); // Debugging the fetched data
+      console.log("Projects Data:", projectsData); // Debugging
       setProjects(projectsData);
     };
 
     fetchProjects();
   }, []);
 
-  // Get unique categories from the projects after data is loaded
+  // Put unique categories from all projects and flatmap into one array of categories
   const categories = [
-    ...new Set(projects.map((project) => project.category)),
+    ...new Set(
+      projects.flatMap((project) => project.category)
+    ),
   ];
 
-  console.log("Categories:", categories); // Debugging the categories list
-
+  // Filter projects based on the selected category, else return all projects
   const filteredProjects = selectedCategory
-    ? projects.filter((project) => project.category === selectedCategory)
+    ? projects.filter((project) =>
+      project.category.includes(selectedCategory)
+    )
     : projects;
 
   return (
@@ -45,25 +48,28 @@ export default function Projects() {
       </section>
       <main className="p-4 flex flex-col justify-center">
         <section>
-          <div className="container mx-auto py-8">
+          <div className="container mx-auto">
             {/* Dropdown categories */}
-            <div className="mb-4">
-              <select
-                value={selectedCategory}
-                onChange={(e) => setSelectedCategory(e.target.value)}
-                className="p-2 border border-gray-300 rounded-md"
-              >
-                <option value="">All Categories</option>
-                {categories.length > 0 && // Ensure categories are present
-                  categories.map((category, index) => (
-                    <option key={index} value={category}>
-                      {category}
-                    </option>
-                  ))}
-              </select>
-            </div>
+            <div className="w-full flex flex-col items-end">
+              <div className="mb-4 max-w-fit">
+                <select
+                  value={selectedCategory}
+                  onChange={(e) => setSelectedCategory(e.target.value)}
+                  className="p-2 border border-gray-300 rounded-md"
+                >
+                  <option value="">All Categories</option>
+                  {categories.length > 0 &&
+                    categories.map((category, index) => (
+                      <option key={index} value={category}>
+                        {category}
+                      </option>
+                    ))}
+                </select>
 
+              </div>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {/* Render projects filtered by choosen category */}
               {filteredProjects.map((project) => (
                 <div
                   key={project.id}
@@ -81,6 +87,7 @@ export default function Projects() {
                   <div className="p-4">
                     <h2 className="font-bold text-xl mb-2">{project?.title}</h2>
                     <p className="text-gray-700">
+                      {/* Substring to simulate excerpt with 250 chars */}
                       {project?.details?.json?.content[0].content[0].value?.substring(
                         0,
                         250
@@ -98,6 +105,7 @@ export default function Projects() {
                 </div>
               ))}
             </div>
+
           </div>
         </section>
       </main>
